210b3681e8e9d240be0eeb99cc4d6bee12c527aa
Lnyx90 <lnyx90prive@gmail.com>
2026-02-02 15:29:12 +0700

    week01: refractor to use val and string templates

--- src/oop_00000108749_JosiahMaximusSuryadi/week01/GradingSystem.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_00000108749_JosiahMaximusSuryadi.week01
 
 fun main(){
-    val name: string = "John Thor"
-    val score: Int = 80
+    val name = "John Thor"
+    val score = 80
 
     println("Nama: $name, Nilai: $score")
 }
