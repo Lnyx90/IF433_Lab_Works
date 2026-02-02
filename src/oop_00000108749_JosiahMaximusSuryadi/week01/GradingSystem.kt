cecbd30d8196b6de3ed4e73109113c7c5bb35c1f
Lnyx90 <lnyx90prive@gmail.com>
2026-02-02 15:26:16 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000108749_JosiahMaximusSuryadi/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000108749_JosiahMaximusSuryadi.week01
+
+fun main(){
+    var name: string = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
