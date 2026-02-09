8e519c8ac7f4b25ba9a69e5276e6ae7fec05bbae
Lnyx90 <lnyx90prive@gmail.com>
2026-02-09 23:43:29 +0700

    week02: challenge refractor primary constructor using default args

--- src/oop_00000108749_JosiahMaximusSuryadi/week02/Student.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week02/Student.kt
@@ -1,7 +1,7 @@
 package oop_00000108749_JosiahMaximusSuryadi.week02
 
 class Student (val name: String, val nim: String, var major: String, var gap: Double = 0.0) {
-    init{
+    init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
@@ -9,6 +9,8 @@ class Student (val name: String, val nim: String, var major: String, var gap: Do
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
-    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated"){
+
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
+}
