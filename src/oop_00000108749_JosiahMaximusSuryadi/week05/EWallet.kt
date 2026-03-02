9ee5bcd722caccecdec305a7134a47690be3d30a
Lnyx90 <lnyx90prive@gmail.com>
2026-03-02 14:13:32 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000108749_JosiahMaximusSuryadi/week05/EWallet.kt
@@ -0,0 +1,16 @@
+package oop_00000108749_JosiahMaximusSuryadi.week05
+
+import kotlin.math.PI
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance > amount){
+            balance -= amount
+            println("Pembayaran Sukses")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double): Double = balance + amount
+}
