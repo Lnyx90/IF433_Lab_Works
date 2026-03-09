9e2150cfb181e9c4551bb7fe6a39662b31137e04
Lnyx90 <lnyx90prive@gmail.com>
2026-03-09 14:06:52 +0700

    week06: (task) create SmartLamp

--- src/oop_00000108749_JosiahMaximusSuryadi/week06/SmartSpeaker.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week06/SmartSpeaker.kt
@@ -1,4 +1,15 @@
 package oop_00000108749_JosiahMaximusSuryadi.week06
 
-class SmartSpeaker {
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable  {
+    override fun turnOff() {
+        println("Mati")
+    }
+
+    override fun turnOn() {
+        println("Nyala")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagi $song dari Spotify.")
+    }
 }
