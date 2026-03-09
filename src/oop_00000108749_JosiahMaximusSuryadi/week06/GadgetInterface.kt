3e448990a770bf4bcf46746808411eebebf6e3cc
Lnyx90 <lnyx90prive@gmail.com>
2026-03-09 13:39:46 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000108749_JosiahMaximusSuryadi/week06/GadgetInterface.kt
@@ -0,0 +1,13 @@
+package oop_00000108749_JosiahMaximusSuryadi.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
