1a3ca6d7f24acc232a99d8114606094fdb15dcc8
Lnyx90 <lnyx90prive@gmail.com>
2026-03-16 13:51:39 +0700

    week07: (trial) trigger private constructor error on NetworkClient

--- src/oop_00000108749_JosiahMaximusSuryadi/week07/NetworkClient.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week07/NetworkClient.kt
@@ -3,14 +3,7 @@ package oop_00000108749_JosiahMaximusSuryadi.week07
 import java.net.URL
 
 class NetworkClient private  constructor(val url:  String){
-    const val BASE_URL = "https://api.umn.ac.id"
-
-    fun createClient(): NetworkClient{
-        println("Membangun NetworkClient dengan BASE_URL: $BASE_URL")
-        return NetworkClient(BASE_URL)
-    }
-
     fun connect () {
-        println("Connecting to $url...")
+        println("Connecting to $url")
     }
 }
