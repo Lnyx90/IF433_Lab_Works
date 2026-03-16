4578a3f133d5f4561fa0d86c71932b4b4734db14
Lnyx90 <lnyx90prive@gmail.com>
2026-03-16 21:45:18 +0700

    week07: (task) create GameItem data class

--- src/oop_00000108749_JosiahMaximusSuryadi/week07/GameModels.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week07/GameModels.kt
@@ -3,3 +3,9 @@ package oop_00000108749_JosiahMaximusSuryadi.week07
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(10), RARE(5), EPIC(3), LEGENDARY(1)
 }
+
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
