89db98cac708b32ce608b9082c0017082fdc6dd6
Lnyx90 <lnyx90prive@gmail.com>
2026-04-20 20:59:03 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000108749_JosiahMaximusSuryadi/week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_00000108749_JosiahMaximusSuryadi.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", -10, 25.0, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 50, -10.0, "CLOSED"),
+        TradeLog("DOTUSDT", "LONG", 50, 8.4, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, -2.1,  "OPEN"),
+        TradeLog("BNBUSDT", "SHORT", 15, 12.0,  "CLOSED")
+    )
+    
+}
