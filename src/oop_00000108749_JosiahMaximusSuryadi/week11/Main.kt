67ca64d2ce83d56ee85af12083ac77b649a545ca
Lnyx90 <lnyx90prive@gmail.com>
2026-05-04 13:00:09 +0700

    week11: test extension functions in main

---
+++ src/oop_00000108749_JosiahMaximusSuryadi/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000108749_JosiahMaximusSuryadi.week11
+
+fun main() {
+    println("=== TEST EXTENTSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
