f8c26e8f88e3fb8743771c5ef4fea210adf33ae4
Lnyx90 <lnyx90prive@gmail.com>
2026-05-04 14:15:16 +0700

    week11: (task) initialize smart home device list

--- src/oop_00000108749_JosiahMaximusSuryadi/week11/SmartHomeDashboard.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week11/SmartHomeDashboard.kt
@@ -3,4 +3,12 @@ package oop_00000108749_JosiahMaximusSuryadi.week11
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
 
+    SmartDevice().apply {
+        name = "Philips Wiz Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }. also {
+        homeDevices.add(it)
+    }
 }
--- src/oop_00000108749_JosiahMaximusSuryadi/week11/SmartHomeModels.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week11/SmartHomeModels.kt
@@ -1,6 +1,6 @@
 package oop_00000108749_JosiahMaximusSuryadi.week11
 
-data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+data class SmartDevice(var name: String = "", var category: String = "", var isOnline: Boolean = false, var powerLoad: Int = 0)
 
 fun SmartDevice.diagnose(): String {
     return "[DIAGNOSTIK] $name | Kategori: $category |Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
