0da983b3db253764ce76719181229402f87a5ab5
Lnyx90 <lnyx90prive@gmail.com>
2026-05-11 12:50:18 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000108749_JosiahMaximusSuryadi/week12/SmartFeeder.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week12/SmartFeeder.kt
@@ -17,4 +17,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(80, currentKibbleStock, false)
+    } catch (e: DispenserJamException) {
+        println("Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error: ${e.message}")
+    } catch (e: Exception) {
+        println("General Error: ${e.message}")
+    }
 }
