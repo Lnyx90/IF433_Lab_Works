b544f25f1cf7492460704353cee049a749c2d76c
Lnyx90 <lnyx90prive@gmail.com>
2026-05-18 13:37:21 +0700

    week13: create save and load list functions

--- src/oop_00000108749_JosiahMaximusSuryadi/week13/StudentSerialization.kt
+++ src/oop_00000108749_JosiahMaximusSuryadi/week13/StudentSerialization.kt
@@ -1,6 +1,7 @@
 package oop_00000108749_JosiahMaximusSuryadi.week13
 import java.io.File
 import java.io.FileNotFoundException
+import java.nio.file.Path
 
 data class Student(val name: String, val age: Int, val gpa: Double)
 
@@ -9,4 +10,19 @@ fun Student.toCsv(): String = "$name,$age,$gpa"
 fun fromCsv(line: String): Student {
     val parts = line.split(",")
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
+}
+
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(students.joinToString("\n") {
+        it.toCsv()
+    })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
 }
